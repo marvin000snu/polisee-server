430e418b1a0b18e444b698a0e6c92f0c618ef393
marvin000snu <57986707+marvin000snu@users.noreply.github.com>
2021-05-30 17:51:10 +0900

    add people data

--- controller/people-controller.js
+++ controller/people-controller.js
@@ -12,6 +12,25 @@ const getAllPeople = async function (req, res, next) {
   }
 };
 
+const getMainAttendData  = async function (req,res,next){
+  const connection = await Pool.getConnection();
+  try{
+    const id = req.params.id;
+    const [[result]] = await connection.query("SELECT * FROM LAWDATA.people where id=?", [id]);
+    res.status(200).json({
+      attend : (result["main-attend"].slice(1,-1).split(",")).map((v)=>{return v.replace(/"/gi,"")}),
+      notAttend : (result["main-notattend"].slice(1,-1).split(",")).map((v)=>{return v.replace(/"/gi,"")}),
+      work : (result["main-work"].slice(1,-1).split(",")).map((v)=>{return v.replace(/"/gi,"")}),
+      home : (result["main-home"].slice(1,-1).split(",")).map((v)=>{return v.replace(/"/gi,"")}),
+    })
+  }catch(err){
+    console.log(err)
+  }finally{
+    connection.release();
+  }
+}
+
 module.exports = {
-  getAllPeople
+  getAllPeople,
+  getMainAttendData
 };
--- router/people-router.js
+++ router/people-router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllPeople} = require("../controller/people-controller");
+const { getAllPeople, getMainAttendData} = require("../controller/people-controller");
 
 router.get("/getAllPeople", getAllPeople);
+router.get("/getMainAttendData/:id",getMainAttendData)
 router.use("/", (err, req, res, next) => {
   next(err);
 });
 
 module.exports = router;
+
