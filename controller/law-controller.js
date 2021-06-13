4c6554b91548660c716dd9d5553591eb143d13be
marvin000snu <57986707+marvin000snu@users.noreply.github.com>
2021-06-13 16:10:12 +0900

    add lead, team

--- controller/law-controller.js
+++ controller/law-controller.js
@@ -1,18 +1,53 @@
 const Pool = require("../utils/mysql");
 
-const search  = async function (req,res,next){
-    const connection = await Pool.getConnection();
-    try{
-        const keyWord = req.params.key;
-        const [result] = await connection.query("SELECT * FROM LAWDATA.law WHERE billName LIKE '%"+keyWord+"%' ")
-        res.status(200).json(result)
-    }catch(err){
-        console.log(err)
-    }finally{
-        connection.release();
-    }
-}
+const search = async function (req, res, next) {
+  const connection = await Pool.getConnection();
+  try {
+    const keyWord = req.params.key;
+    const [result] = await connection.query(
+      "SELECT * FROM LAWDATA.law WHERE billName LIKE '%" + keyWord + "%' "
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.release();
+  }
+};
 
-module.exports ={
-    search
-}
+const lead = async function (req, res, next) {
+  const connection = await Pool.getConnection();
+  try {
+    const name = req.params.name;
+    const [result] = await connection.query(
+      "SELECT * FROM LAWDATA.law WHERE law.lead LIKE '%" + name + "%'"
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.release();
+  }
+};
+
+const team = async function (req, res, next) {
+  const connection = await Pool.getConnection();
+  try {
+    const name = req.params.name;
+    const [result] = await connection.query(
+      "SELECT * FROM LAWDATA.law WHERE team LIKE '%" + name + "%'"
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.release();
+  }
+};
+
+
+module.exports = {
+  search,
+  lead,
+  team
+};
--- router/law-router.js
+++ router/law-router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { search} = require("../controller/law-controller");
+const { search, lead, team } = require("../controller/law-controller");
 
 router.get("/search/:key", search);
+router.get("/lead/:name", lead);
+router.get("/team/:name", team);
 router.use("/", (err, req, res, next) => {
   next(err);
 });
 
 module.exports = router;
-
