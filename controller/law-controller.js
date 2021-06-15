ee21c2b25d6495568d7c2af3f12cf4fc475ff917
marvin000snu <57986707+marvin000snu@users.noreply.github.com>
2021-06-16 03:06:11 +0900

    1

--- controller/law-controller.js
+++ controller/law-controller.js
@@ -49,14 +49,17 @@ const vote = async function (req, res, next) {
   const connection = await Pool.getConnection();
   try {
     const id = req.params.id;
-    const [[result]] = await connection.query(
-      "SELECT `agree`, `disagree`, `notattend`, `drop` FROM LAWDATA.law WHERE billNo  =  ?", [id]);
+    const [
+      [result]
+    ] = await connection.query(
+      "SELECT `agree`, `disagree`, `notattend`, `drop` FROM LAWDATA.law WHERE billNo  =  ?",
+      [id]
+    );
     res.status(200).json({
-      agree : JSON.parse(result.agree),
-      disagree : JSON.parse(result.disagree),
-      notAttend : JSON.parse(result.notattend),
-      drop : JSON.parse(result.drop),
-      
+      agree: JSON.parse(result.agree),
+      disagree: JSON.parse(result.disagree),
+      notAttend: JSON.parse(result.notattend),
+      drop: JSON.parse(result.drop)
     });
   } catch (err) {
     console.log(err);
@@ -67,18 +70,35 @@ const vote = async function (req, res, next) {
 
 const preview = async function (req, res, next) {
   const connection = await Pool.getConnection();
-  try{
-    const [result] = await connection.query("SELECT * FROM LAWDATA.law order by rand() limit 12;")
+  try {
+    const [result] = await connection.query(
+      "SELECT * FROM LAWDATA.law order by rand() limit 12;"
+    );
     res.status(200).json(result);
-  }catch (err) {
+  } catch (err) {
     console.log(err);
-  }finally{
+  } finally {
     connection.release();
   }
-}
+};
 
-
-
+const today = async function (req, res, next) {
+  const connection = await Pool.getConnection();
+  try {
+    const [result] = await connection.query(
+      "SELECT law.group, COUNT(billNo) AS cnt FROM LAWDATA.law GROUP BY law.group;"
+    );
+    const [response] = await connection.query(
+      "SELECT law.group, COUNT(billNo) AS cnt FROM LAWDATA.law where generalResult='수정가결' or generalResult='원안가결' or generalResult='대안반영폐기' GROUP BY law.group "
+    );
+    console.log(result);
+    res.status(200).json({ result: result, response: response });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.release();
+  }
+};
 
 module.exports = {
   search,
@@ -86,4 +106,5 @@ module.exports = {
   team,
   vote,
   preview,
+  today
 };
